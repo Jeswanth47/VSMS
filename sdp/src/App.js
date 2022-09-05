19f77794f89481267b615882e173b498b4f95807
Jeswanth <jeswanthnaidu7@gmail.com>
2022-09-05 16:08:44 +0530

    Added login and signup forms

--- sdp/src/App.js
+++ sdp/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Vehicle Service</h1>
+      <p>loremljvdnlvdnldvnlndvldv ldv ldnvmdvldvnldvnldvnlnvdklnvdkn</p>
     </div>
   );
 }
